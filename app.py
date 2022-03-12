339260121f942a7737cc2425dcb3159cd0c4bd6c
Mario Mendoza <97628292+mjaquez671@users.noreply.github.com>
2022-03-11 20:18:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
-
+  
 app = Flask(__name__)
-
 @app.route("/")
-def home_func():
-    return render_template(r"home.html")
+def home():
+    return(render_template(r'index.html'))
+if __name__ =="__main__":
+    app.run(host = '0.0.0.0', port =8000)
